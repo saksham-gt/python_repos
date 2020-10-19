2473423709ceb75fd178f36f245f94a7b3cf10d8
Saksham <sakshamgupta391@gmail.com>
2020-10-19 13:30:09 +0530

    creating conflict

--- abcdCallStack.py
+++ abcdCallStack.py
@@ -11,18 +11,19 @@ def a():
     b()
     d()
     print('a() returns.')
-    
+
 def b():
     print('b() starts!')
     c()
     print('b() returns.')
-    
+
 def c():
     print('c() starts!')
     print('c() returns.')
-    
+
 def d():
     print('d() starts!')
     print('d() returns.')
-    
+
 a()
+print('Writing to create conflict')
